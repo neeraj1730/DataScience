9a2ace2288c5643c8dc9a2e96b31b1d7cdb87208
neeraj1730 <70251375+neeraj1730@users.noreply.github.com>
2020-08-26 22:44:29 +0530

    Add files via upload

---
+++ Basic_stat.R
@@ -0,0 +1,38 @@
+exp(1)
+x=1
+sqrt(10)
+
+(6*3)+5/10
+vect_cahr<-c(10,20)
+vect_cahr
+
+x <- c(1,2,3,5)
+x*5
+x+(x+1)
+vec<-c('a','nbn')
+vect_x<-10
+vect_y <-20
+vect_add <- vect_x+vect_y 
+vect_add
+
+df<-data.frame(x=c(1,2,3),y=c('a','b','c'))
+q1 = df[1,1]
+q1
+q2 = df[c(1,3),c(1,2)]
+q2
+
+# To exercise of pre defined dataset
+airquality<- datasets::airquality
+head(airquality,10)
+tail(airquality,10)
+
+airquality[,c(1,2)]
+df<-airquality[,-6]
+
+
+
+
+
+
+
+
